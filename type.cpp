c802e3776cc48a724ea0c72981d623bde7ac392f
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2017-11-19 16:23:34 +0600

    Create type.cpp

---
+++ type.cpp
@@ -0,0 +1,40 @@
+Hi everybody,
+I collected some advanced graph problems. Maybe you need them for exercises.
+
+Max-Flow
+00563 Crimewave ------------------------ (Vertices have capacity. Edges have not.)
+00820 Internet Bandwidth
+10330 Power Transmission -------------- (Vertices have capacity.)
+10511 Councilling ------------------------- (Vertices have capacity.)
+10779 Collectors Problem ---------------- (Vertices have capacity.)
+
+Min-Cost Max-Flow
+10594 Data Flow
+
+Bipartite Matching
+00259 Software Allocation
+00663 Sorting Slides
+00670 The Dog Task
+00753 A plug for Unix
+10080 Gopher II
+10092 The Problem with the Problemsetter
+10243 Fire! Fire!! Fire!!!
+10418 Hyper Toy Soldiers
+10735 Euler Circuit
+10984 Double NP-hard
+
+Min-Cost Bipartite-Matching
+10122 Mysterious Mountain
+10804 Gopher Strategy ------------------- (We can simply use Binary Search for the Min-Cost.)
+10746 Crime Wave - The Sequel
+10888 Warehouse ------------------------- (Dynamic Programming is allowed.)
+
+Matching
+10380 Shogi Tournament
+
+Min-Cost Matching
+10911 Forming Quiz Teams ------------- (Dynamic Programming is allowed.)
+
+Min-Cut
+10480 Sabotage --------------------------- (Min-Cut Max-Flow Theorem can be used.)
+10989 Bomb, Divide and Conquer
